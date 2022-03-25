5d45112e0e0ba4beec6645167bdf320891e8856b
Vasyl Kitsula <kitsulavasyl@gmail.com>
2022-03-25 16:47:07 +0200

    edit in controller

--- AnnouncementWebApi/Controllers/AnnouncementController.cs
+++ AnnouncementWebApi/Controllers/AnnouncementController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+using AnnouncementWebApi;
 
 namespace AnnouncementWebApi.Controllers
 {
@@ -13,5 +16,12 @@ namespace AnnouncementWebApi.Controllers
         // в інтерфейсах створити інтерфейс і оголосити в ньому методи, які потрібні для реалізація завдання CRUD
         // в імплемент має бути реалізація методів() EditAnnoun, AddAn, DelAnn..
         // GitHub  і контролери
+
+        private static List<Announcement> announcements = new List<Announcement>(new[]
+        {
+            new Announcement() {Id = 1, Title = "First announcement", Description = "Something in announcement, ets....", CreatedDate = DateTime.Now},
+            new Announcement() {Id = 2, Title = "Second announcement", Description = "This is a different from other each", CreatedDate = DateTime.Now},
+            new Announcement() {Id = 2, Title = "third announcement", Description = "Something in announcement, ets....", CreatedDate = DateTime.Now},
+        }); 
     }
 }
