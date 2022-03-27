809c363d87b7322baf035b0674bb10e1cc73f29e
Vasyl Kitsula <102357007+VasylKits@users.noreply.github.com>
2022-03-27 20:13:05 +0300

    Revert "Develop"

--- AnnouncementWebApi/AddedAnnouncement.cs
+++ AnnouncementWebApi/AddedAnnouncement.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace AnnouncementWebApi.Models
+namespace AnnouncementWebApi
 {
-    public class DelAnnouncement
+    public class AddedAnnouncement
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
 
+        public List<Announcement> AnnouncementList = new();
     }
 }
---
+++ AnnouncementWebApi/Announcement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnnouncementWebApi
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public AddedAnnouncement AddAnnouncement(int id, string title, string description, DateTime createdDate)
+        {
+            AddedAnnouncement newAnn = new() { Id = id, Title = title, Description = description, CreatedDate = createdDate };
+            return newAnn;
+        }
+
+        public Announcement EditAnnouncement(Announcement announcement)
+        {
+            return announcement;
+        }
+        public Announcement DeleteAnnouncement(Announcement announcement)
+        {
+            return announcement;
+        }
+    }
+}
