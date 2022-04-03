dd09d282910f175a3f3178f103a8fcfdff78cac2
Vasyl Kitsula <kitsulavasyl@gmail.com>
2022-04-03 18:18:16 +0300

    Remark, add Async

--- AnnouncementWebApi/Controllers/AnnouncementController.cs
+++ AnnouncementWebApi/Controllers/AnnouncementController.cs
@@ -3,7 +3,6 @@ using AnnouncementWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
