b46a4ec0b5bdfd1545c043a116a986556bf68485
Vasyl Kitsula <kitsulavasyl@gmail.com>
2022-04-10 12:50:51 +0300

    Finished AnnouncementService

--- AnnouncementWebApi/Controllers/AnnouncementController.cs
+++ AnnouncementWebApi/Controllers/AnnouncementController.cs
@@ -5,15 +5,12 @@ using System.Threading.Tasks;
 
 namespace AnnouncementWebApi.Controllers
 {
-    //заінжектити сервіси через конструктор
     [ApiController]
     [Route("[controller]")]
 
     public class AnnouncementController : ControllerBase
     {
-        //private readonly DatabaseContext _myDbContext = new();
-
-        readonly IAnnouncementService _announcementService = null;
+        readonly IAnnouncementService _announcementService;
 
         public AnnouncementController(IAnnouncementService announcementService)
         {
@@ -30,12 +27,6 @@ namespace AnnouncementWebApi.Controllers
         public async Task<IActionResult> GetByIdAsync(int id)
         {
             return Ok(await _announcementService.GetByIdAsync(id));
-        //    var announcement = await _myDbContext.Announcements.FindAsync(id);
-        //    if (announcement == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //   return Ok(announcement);
         }
 
         [HttpPost]
@@ -48,25 +39,6 @@ namespace AnnouncementWebApi.Controllers
         public async Task<IActionResult> EditAnnouncementAsync(EditAnnouncement editAnnouncement)
         {
             return Ok(await _announcementService.EditAnnouncementAsync(editAnnouncement));
-            //    var editAnn = _myDbContext.Announcements.SingleOrDefault(a => a.Id == editAnnouncement.Id);
-            //    if (editAnn == null)
-            //    {
-            //        return NotFound();
-            //    }
-            //    {
-            //        editAnn.Title = editAnnouncement.Title;
-            //        editAnn.Description = editAnnouncement.Description;
-            //        editAnn.EditDate = DateTime.Now;
-            //    }
-            //    try
-            //    {
-            //        await _myDbContext.SaveChangesAsync();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        return BadRequest($"{ex.Message}");
-            //    }
-            //    return Ok(editAnnouncement);
         }
 
         [HttpDelete]
@@ -74,22 +46,12 @@ namespace AnnouncementWebApi.Controllers
         {
             await _announcementService.DeleteAnnouncementAsync(id);
             return Ok($"Successful!\nAnnouncement with id={id} was deleted!");
-
-            //var delAnnouncement = _myDbContext.Announcements.SingleOrDefault(a => a.Id == id);
-            //if (delAnnouncement == null)
-            //{
-            //    return NotFound();
-            //}
-            //_myDbContext.Announcements.Remove(delAnnouncement);
-            //await _myDbContext.SaveChangesAsync();
-            //return Ok($"Successful!\nAnnouncement with id={id} was deleted!");
         }
 
         [HttpGet("TOP")]
         public async Task<IActionResult> ShowTopThreeAnnouncementsAsync()
         {
             return Ok(await _announcementService.ShowTopThreeAnnouncementsAsync());
-            //return Ok(await _myDbContext.Announcements.Where(a => a.Title.Contains("announcement")).OrderBy(a => a.Id).Take(3).ToListAsync());
         }
     }
 }
--- AnnouncementWebApi/Services/Implementations/AnnouncementService.cs
+++ AnnouncementWebApi/Services/Implementations/AnnouncementService.cs
@@ -12,28 +12,10 @@ namespace AnnouncementWebApi.Services.Implementations
 {
     public class AnnouncementService : IAnnouncementService
     {
-        //заінжектити використовуючи констуктор класу 
-        readonly DatabaseContext _myDbContext = null;
+        readonly DatabaseContext _myDbContext;
         public AnnouncementService(DatabaseContext myDbContext)
         {
             _myDbContext = myDbContext;
-        }
-
-        public async Task<AnnouncementResponse> AddAnnouncementAsync(NewAnnouncement newAnnouncement)
-        {
-            Announcement newAnn = new() { Title = newAnnouncement.Title, Description = newAnnouncement.Description };
-            _myDbContext.Announcements.Add(newAnn);
-            var announcementResponse = new AnnouncementResponse();
-            try
-            {
-                await _myDbContext.SaveChangesAsync();
-                announcementResponse = new AnnouncementResponse() { Title = newAnn.Title, Description = newAnn.Description };
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(message: $"{ex.Message}");
-            }
-            return announcementResponse;
         }
 
         public async Task<List<AnnouncementResponse>> GetAnnouncementAsync()
@@ -42,44 +24,73 @@ namespace AnnouncementWebApi.Services.Implementations
             var responceList = new List<AnnouncementResponse>();
             foreach (var item in varList)
             {
-                var announcementResponse = new AnnouncementResponse() { Title = item.Title, Description = item.Description };
+                var announcementResponse = new AnnouncementResponse() {Id = item.Id, Title = item.Title, Description = item.Description, CreatedDate = item.CreatedDate };
                 responceList.Add(announcementResponse);
             }
             return responceList;
         }
-
-        public async Task<AnnouncementResponse> EditAnnouncementAsync(EditAnnouncement editAnnouncement)
+        public async Task<AnnouncementResponse> GetByIdAsync(int id)
         {
-            var editAnn = _myDbContext.Announcements.SingleOrDefault(a => a.Id == editAnnouncement.Id);
-            if (editAnn == null)
+            var announcementResponse = new AnnouncementResponse();
+            var announcement = await _myDbContext.Announcements.FindAsync(id);
+            if (announcement == null)
             {
-                throw new NotImplementedException();
+                throw new Exception();
             }
             {
-                editAnn.Title = editAnnouncement.Title;
-                editAnn.Description = editAnnouncement.Description;
-                editAnn.EditDate = DateTime.Now;
+                announcementResponse.Id = announcement.Id;
+                announcementResponse.Title = announcement.Title;
+                announcementResponse.Description = announcement.Description;
+                announcementResponse.CreatedDate = announcement.CreatedDate;
             }
+            return announcementResponse;
+        }
+        public async Task<AnnouncementResponse> AddAnnouncementAsync(NewAnnouncement newAnnouncement)
+        {
+            Announcement newAnn = new() { Title = newAnnouncement.Title, Description = newAnnouncement.Description };
+            _myDbContext.Announcements.Add(newAnn);
             try
             {
                 await _myDbContext.SaveChangesAsync();
             }
             catch (Exception ex)
             {
-                throw new NotImplementedException();
+                throw new Exception(message: $"{ex.Message}");
             }
-            throw new NotImplementedException();
+            var announcementResponse = new AnnouncementResponse() { Id = newAnn.Id, Title = newAnn.Title, Description = newAnn.Description, CreatedDate = newAnn.CreatedDate };
+            return announcementResponse;
         }
 
-        public async Task<AnnouncementResponse> GetByIdAsync(int id)
+        public async Task<AnnouncementResponse> EditAnnouncementAsync(EditAnnouncement editAnnouncement)
         {
-            var announcement = await _myDbContext.Announcements.FindAsync(id);
-            if (announcement == null)
+            AnnouncementResponse announcementResponse = new();
+            var editAnn = await _myDbContext.Announcements.SingleOrDefaultAsync(a => a.Id == editAnnouncement.Id);
+
+            if (editAnn == null)
             {
                 throw new Exception();
             }
-            return announcement;
-            throw new NotImplementedException();
+
+            {
+                editAnn.Title = editAnnouncement.Title;
+                editAnn.Description = editAnnouncement.Description;
+                editAnn.EditDate = DateTime.Now;
+            }
+
+            try
+            {
+                await _myDbContext.SaveChangesAsync();
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(message: $"{ex.Message}");
+            }
+            announcementResponse.Id = editAnn.Id;
+            announcementResponse.Title = editAnn.Title;
+            announcementResponse.Description = editAnn.Description;
+            announcementResponse.CreatedDate = editAnn.CreatedDate;
+            return announcementResponse;
         }
 
         public async Task<int?> DeleteAnnouncementAsync(int id)
@@ -87,7 +98,7 @@ namespace AnnouncementWebApi.Services.Implementations
             var delAnnouncement = _myDbContext.Announcements.SingleOrDefault(a => a.Id == id);
             if (delAnnouncement == null)
             {
-                return null;
+                throw new Exception();
             }
             _myDbContext.Announcements.Remove(delAnnouncement);
             await _myDbContext.SaveChangesAsync();
@@ -96,7 +107,14 @@ namespace AnnouncementWebApi.Services.Implementations
 
         public async Task<List<AnnouncementResponse>> ShowTopThreeAnnouncementsAsync()
         {
-            throw new NotImplementedException();
+            var varList = await _myDbContext.Announcements.Where(a => a.Title.Contains("announcement")).OrderBy(a => a.Id).Take(3).ToListAsync();
+            var responceList = new List<AnnouncementResponse>();
+            foreach (var item in varList)
+            {
+                var announcementResponse = new AnnouncementResponse() { Id = item.Id, Title = item.Title, Description = item.Description, CreatedDate = item.CreatedDate };
+                responceList.Add(announcementResponse);
+            }
+            return responceList;
         }
     }
 }
